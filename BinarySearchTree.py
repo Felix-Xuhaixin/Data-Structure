c2b7c4bbb7a25241a5065b807a021ad1fffa3a28
Felix <felix.xuhaixin@gmail.com>
2026-04-08 23:59:26 +0800

    complet insert

---
+++ BinarySearchTree.py
@@ -0,0 +1,115 @@
+# property: Every key to the left of node is smaller than node, 
+#  every key to the right of node is larger than node
+
+class Node:
+    
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.parent = None  
+        self.key = key
+        self.value = None
+
+    def  __repr__(self):
+        print (f"({self.key},{self.value})")
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    def __contains__(self,key):
+        current_node = self.root
+
+        while current_node is not None:
+            if current_node < key:
+                current_node = current_node.left
+            elif current_node > key:
+                current_node = current_node.right
+            else:
+                return True
+        
+        return False
+        
+
+    def __iter__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def insert(self, key, value):
+        if self.root is None:
+            self.root = Node(key)
+            self.root.value = value
+        else:
+            current_node = self.root
+            while True:
+                if current_node.key > key:
+                    if current_node.left is None:
+                        current_node.left = Node(key)
+                        current_node.left.value = value
+                        current_node.left.parent = current_node
+                        break
+                    else:
+                        current_node = current_node.left
+                elif current_node.key < key:
+                    if current_node.right is None:
+                        current_node.right = Node(key)
+                        current_node.right.value = value
+                        current_node.right.parent = current_node
+                        break
+                    else:
+                        current_node = current_node.right
+                else:
+                    current_node.value = value
+                    break
+                    
+
+                    
+                
+                    
+
+    def search(self, key):
+        current_node =  self.root
+
+        while True:         
+            if current_node is None or current_node.key == key:
+                return current_node
+            elif key < current_node.key:
+                if current_node.left is None:
+                    return None
+                else:
+                    current_node = current_node.left
+            else:
+                if current_node.right is None:
+                    return None
+                else:
+                    current_node = current_node.right
+                    
+                
+
+
+    def delete(self,key):
+        pass
+
+    def traverse(self, order):
+        pass
+
+    def _delete(self,key):  # helper fuction to delete() function
+        pass
+    
+    def _successor(self,node):
+        pass
+    
+    def _predecessor(self,node):
+        pass
+
+    def _in_order_traversal(self):
+        pass
+ 
+    def _pre_order_traversal(self):
+        pass
+    
+    def _post_order_traversal(self):
+        pass 
